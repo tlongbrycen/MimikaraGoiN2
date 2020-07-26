7bc5696ff8dc6217c94b8ac317f9be5ab22c0df3
tlong <tlong@brycen.co.jp>
2020-07-26 19:58:35 +0900

    2nd

--- MimikaraGoiN2/Form1.Designer.cs
+++ MimikaraGoiN2/Form1.Designer.cs
@@ -33,6 +33,23 @@ namespace MimikaraGoiN2
             this.comboBoxUnit = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.buttonUnitSelect = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxVie = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBoxD = new System.Windows.Forms.CheckBox();
+            this.checkBoxC = new System.Windows.Forms.CheckBox();
+            this.checkBoxB = new System.Windows.Forms.CheckBox();
+            this.checkBoxA = new System.Windows.Forms.CheckBox();
+            this.buttonFuriganaSelect = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxWord = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxEng = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxExample = new System.Windows.Forms.TextBox();
+            this.buttonNextQuestion = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // comboBoxUnit
@@ -62,16 +79,173 @@ namespace MimikaraGoiN2
             this.buttonUnitSelect.UseVisualStyleBackColor = true;
             this.buttonUnitSelect.Click += new System.EventHandler(this.buttonUnitSelect_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tiếng Việt";
+            // 
+            // textBoxVie
+            // 
+            this.textBoxVie.Location = new System.Drawing.Point(105, 50);
+            this.textBoxVie.Name = "textBoxVie";
+            this.textBoxVie.Size = new System.Drawing.Size(413, 22);
+            this.textBoxVie.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "フリガナ";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBoxD);
+            this.panel1.Controls.Add(this.checkBoxA);
+            this.panel1.Controls.Add(this.checkBoxC);
+            this.panel1.Controls.Add(this.checkBoxB);
+            this.panel1.Location = new System.Drawing.Point(105, 78);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(317, 112);
+            this.panel1.TabIndex = 10;
+            // 
+            // checkBoxD
+            // 
+            this.checkBoxD.AutoSize = true;
+            this.checkBoxD.Location = new System.Drawing.Point(13, 84);
+            this.checkBoxD.Name = "checkBoxD";
+            this.checkBoxD.Size = new System.Drawing.Size(39, 19);
+            this.checkBoxD.TabIndex = 14;
+            this.checkBoxD.Text = "D";
+            this.checkBoxD.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxC
+            // 
+            this.checkBoxC.AutoSize = true;
+            this.checkBoxC.Location = new System.Drawing.Point(13, 59);
+            this.checkBoxC.Name = "checkBoxC";
+            this.checkBoxC.Size = new System.Drawing.Size(39, 19);
+            this.checkBoxC.TabIndex = 13;
+            this.checkBoxC.Text = "C";
+            this.checkBoxC.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxB
+            // 
+            this.checkBoxB.AutoSize = true;
+            this.checkBoxB.Location = new System.Drawing.Point(13, 34);
+            this.checkBoxB.Name = "checkBoxB";
+            this.checkBoxB.Size = new System.Drawing.Size(39, 19);
+            this.checkBoxB.TabIndex = 12;
+            this.checkBoxB.Text = "B";
+            this.checkBoxB.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxA
+            // 
+            this.checkBoxA.AutoSize = true;
+            this.checkBoxA.Location = new System.Drawing.Point(13, 9);
+            this.checkBoxA.Name = "checkBoxA";
+            this.checkBoxA.Size = new System.Drawing.Size(38, 19);
+            this.checkBoxA.TabIndex = 11;
+            this.checkBoxA.Text = "A";
+            this.checkBoxA.UseVisualStyleBackColor = true;
+            // 
+            // buttonFuriganaSelect
+            // 
+            this.buttonFuriganaSelect.Location = new System.Drawing.Point(443, 78);
+            this.buttonFuriganaSelect.Name = "buttonFuriganaSelect";
+            this.buttonFuriganaSelect.Size = new System.Drawing.Size(75, 23);
+            this.buttonFuriganaSelect.TabIndex = 11;
+            this.buttonFuriganaSelect.Text = "選択";
+            this.buttonFuriganaSelect.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "単語";
+            // 
+            // textBoxWord
+            // 
+            this.textBoxWord.Location = new System.Drawing.Point(105, 207);
+            this.textBoxWord.Name = "textBoxWord";
+            this.textBoxWord.Size = new System.Drawing.Size(413, 22);
+            this.textBoxWord.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 248);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 15);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "English";
+            // 
+            // textBoxEng
+            // 
+            this.textBoxEng.Location = new System.Drawing.Point(105, 245);
+            this.textBoxEng.Name = "textBoxEng";
+            this.textBoxEng.Size = new System.Drawing.Size(413, 22);
+            this.textBoxEng.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 289);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 15);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "例";
+            // 
+            // textBoxExample
+            // 
+            this.textBoxExample.Location = new System.Drawing.Point(105, 286);
+            this.textBoxExample.Name = "textBoxExample";
+            this.textBoxExample.Size = new System.Drawing.Size(413, 22);
+            this.textBoxExample.TabIndex = 17;
+            // 
+            // buttonNextQuestion
+            // 
+            this.buttonNextQuestion.Location = new System.Drawing.Point(443, 112);
+            this.buttonNextQuestion.Name = "buttonNextQuestion";
+            this.buttonNextQuestion.Size = new System.Drawing.Size(75, 23);
+            this.buttonNextQuestion.TabIndex = 18;
+            this.buttonNextQuestion.Text = "次";
+            this.buttonNextQuestion.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(559, 262);
+            this.ClientSize = new System.Drawing.Size(546, 401);
+            this.Controls.Add(this.buttonNextQuestion);
+            this.Controls.Add(this.textBoxExample);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxEng);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxWord);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonFuriganaSelect);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxVie);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.buttonUnitSelect);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBoxUnit);
             this.Name = "Form1";
-            this.Text = "耳から覚える語彙N2";
+            this.Text = "日本語勉強";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,6 +256,22 @@ namespace MimikaraGoiN2
         private System.Windows.Forms.ComboBox comboBoxUnit;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button buttonUnitSelect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxVie;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBoxD;
+        private System.Windows.Forms.CheckBox checkBoxA;
+        private System.Windows.Forms.CheckBox checkBoxC;
+        private System.Windows.Forms.CheckBox checkBoxB;
+        private System.Windows.Forms.Button buttonFuriganaSelect;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxWord;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxEng;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxExample;
+        private System.Windows.Forms.Button buttonNextQuestion;
     }
 }
 
--- MimikaraGoiN2/Form1.cs
+++ MimikaraGoiN2/Form1.cs
@@ -4,35 +4,123 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace MimikaraGoiN2
 {
     public partial class Form1 : Form
     {
         private int selectedUnit;
-        
+        private Excel.Application xlApp;
+        private Excel.Workbook xlWorkbook;
+        private Excel._Worksheet xlWorksheet;
+        private Excel.Range xlRange;
+        private List<string> lstWords;
+        private List<string> lstExample;
+        private List<string> lstEng;
+        private List<string> lstVie;
+        private List<string> lstFurigana;
+
         public Form1()
         {
             InitializeComponent();
             comboBoxUnitLoad();
+            eventInit();
+            xlApp = new Excel.Application();
+            xlWorkbook = xlApp.Workbooks.Open(@"C:\Users\tlong\source\repos\MimikaraGoiN2\MimikaraGoiN2\bin\x64\Release\listN2.xlsx");
+            this.lstWords = new List<string>();
+            this.lstEng = new List<string>();
+            this.lstVie = new List<string>();
+            this.lstExample = new List<string>();
+            this.lstFurigana = new List<string>();
+            this.buttonUnitSelect.Enabled = false;
+            this.textBoxExample.Multiline = true;
+        }
+
+        private void eventInit()
+        {
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(HMI_FormClosing);
+            this.comboBoxUnit.SelectedIndexChanged += new EventHandler(unitBoxSelected);
         }
         private void comboBoxUnitLoad()
         {
-            this.comboBoxUnit.Text = "一";
+            this.comboBoxUnit.Text = "選択";
             String[] unitList = { "一", "二" };
             comboBoxUnit.Items.AddRange(unitList);
+        }
+
+        private void unitBoxSelected(Object sender, EventArgs e)
+        {
+            buttonUnitSelect.Enabled = true;
         }
 
         private void buttonUnitSelect_Click(object sender, EventArgs e)
         {
             selectedUnit = comboBoxUnit.SelectedIndex;
-            //MessageBox.Show(selectedUnit.ToString());
+            //MessageBox.Show(selectedUnit.ToString()); // Debug
+            importDataXls();
+            
         }
 
-        private void importData()
+        private void importDataXls()
+        {
+            xlWorksheet = xlWorkbook.Sheets[selectedUnit + 1];
+            xlRange = xlWorksheet.UsedRange;
+            enlistXls(lstWords, 1);
+            enlistXls(lstExample, 2);
+            enlistXls(lstEng, 3);
+            enlistXls(lstVie, 4);
+            enlistXls(lstFurigana, 5);
+            //MessageBox.Show(lstFurigana[5]); // Debug
+            MessageBox.Show("はじめましう");
+        }
+
+        private void enlistXls(List<string> list, int colIdx)
+        {
+            String str = "一";
+            int rowIdx = 1;
+            do
+            {
+                str = xlWorksheet.Cells[rowIdx, colIdx].Text;
+                list.Add(str);
+                rowIdx = rowIdx + 1;
+            }
+            while (!str.Equals("x"));
+        }
+
+        private void clearDataXls()
+        {
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //rule of thumb for releasing com objects:
+            //  never use two dots, all COM objects must be referenced and released individually
+            //  ex: [somthing].[something].[something] is bad
+
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+        }
+
+        private void HMI_FormClosing(object sender, EventArgs e)
+        {
+            clearDataXls();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
